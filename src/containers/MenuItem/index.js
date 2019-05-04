79c991bbd912e3ddf5fe53a71fd3faa50864f1e0
Le Tuan Anh <lekoi9x@gmail.com>
2019-05-04 16:31:07 +0700

    remove redundant code

--- src/containers/MenuItem/index.js
+++ src/containers/MenuItem/index.js
@@ -2,23 +2,8 @@ import React from 'react'
 import "./index.css"
 
 class MenuItem extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isShow: false
-    }
-    this.toggleHover = this.toggleHover.bind(this)
-  }
- 
-  toggleHover() {
-    this.setState((prev) => ({
-      isShow: !prev.isShow
-    }))
-  }
-
   render() {
     const { label, subMenu } = this.props;
-    const { isShow } = this.state;
     return (
       <li>
         { 
@@ -29,7 +14,7 @@ class MenuItem extends React.Component {
               subMenu && <i className="fas fa-angle-right"></i>
             }
             {
-              subMenu && <ul className={`tree-menu__submenu tree-menu__submenu--hidden ${isShow ? 'tree-menu__submenu--show' : ''}`}>{subMenu}</ul>
+              subMenu && <ul className={`tree-menu__submenu tree-menu__submenu--hidden`}>{subMenu}</ul>
             }
           </div>
         }
